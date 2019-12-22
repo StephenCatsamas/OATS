8f09d84fcb7bbffe1c0225ddb8c9579d6a9e5def
Stephen Catsamas <seca8193@gmail.com>
2019-12-22 13:19:02 +1100

    started work on AST tree interpreter

--- Compiler.py
+++ Compiler.py
@@ -15,7 +15,8 @@ class nodeAST:
     def __iter__(self):
         return iter(self.children)
     
-    def __init__(self,name):
+    def __init__(self,type,name = None):
+        self.type = type
         self.name = name
         self.children = list()
         
@@ -23,24 +24,60 @@ class nodeAST:
             for child in self.children:
                 for i in range(nodeAST.depth):
                     print("  ", end ="")
-                print("--", child.name)
+                print("--", child.type, child.name)
+                
                 nodeAST.depth += 1
                 child.printChildren()
                 nodeAST.depth -= 1
     
-    def addChild(self,name):
-        child = nodeAST(name)
+    def addChild(self,type,name = None):
+        child = nodeAST(type,name)
         self.children.append(child)
-        
+idx = 0
+def printAST(AST):
+    for child in AST:
+        text = getPrint(child,idx)
+        if text != None:
+            outlst.insert(text,idx)
+            idx += 1
+        printAST(child)
 
+def getPrint(node):
+    if node.type == "op":
+        if node.name == "set":
+            return "="
+        if node.name == "in":
+            return "in"
+    if node.type == "var":
+        return node.name
+    if node.type == "basic":
+        return node.name
+    if node.type == "ctrl":
+        if node.name == "for":
+            return node.name
+    if node.type == "func":
+        return node.name
+        
+    
+        
+    return None
+    
+    
 root = nodeAST("root")
 
-root.addChild("fnoo")
-root.addChild("fboo")
-root[0].addChild("xnoo")
-root[0][0].addChild("znoo")
+root.addChild("op", "set")
+root[0].addChild("var","i")
+root[0].addChild("basic","0")
+root.addChild("ctrl", "for")
+root[1].addChild("op", "in")
+root[1][0].addChild("var","i")
+root[1][0].addChild("func", "range")
+root[1][0][1].addChild("basic", "7")
+root[1].addChild("func", "print")
+root[1][1].addChild("var","i")
 
 root.printChildren()
-
-
+print("##########")
+printAST(root)
+print(outlst)
     
