d67a722aaf205454776e1a9e1a17b84aab8cdfed
Stephen Catsamas <seca8193@gmail.com>
2019-12-22 20:46:48 +1100

    changed some names in house keeping added if statement and == to langauge interpreter

--- Compiler.py
+++ Compiler.py
@@ -1,155 +1,173 @@
-
 fp = "Example.oat"
 op = "comp.py"
 
-# with open(fp, 'r') as srcf:
-    # src = srcf.read()
-    # print(src)
 
-class nodeAST:
-    depth = 0
-    
+# with open(fp, 'r') as srcf:
+#     src = srcf.read()
+#     print(src)
+
+class AST:
+
     def __getitem__(self, key):
-        return self.children[key]
-    
+        return self.leaves[key]
+
     def __iter__(self):
-        return iter(self.children)
-    
-    def __init__(self,type,name = None):
+        return iter(self.leaves)
+
+    def __init__(self, type, name=None):
         self.type = type
         self.name = name
-        self.children = list()
-        
+        self.leaves = list()
+        self.printDepth = 0
+
     def printChildren(self):
-            for child in self.children:
-                for i in range(nodeAST.depth):
-                    print("  ", end ="")
-                print("--", child.type, child.name)
-                
-                nodeAST.depth += 1
-                child.printChildren()
-                nodeAST.depth -= 1
-    
-    def addChild(self,type,name = None):
-        child = nodeAST(type,name)
-        self.children.append(child)
+        for leaf in self.leaves:
+            for i in range(self.printDepth):
+                print("  ", end="")
+            print("--", leaf.type, leaf.name)
 
-def printAST(AST):
-        stk = printOdr(AST)
-        for node in stk:
-            if node == AST:
-                doPrint(node)
+            self.printDepth += 1
+            leaf.printChildren()
+            self.printDepth -= 1
+
+    def addChild(self, type, name=None):
+        leaf = AST(type, name)
+        self.leaves.append(leaf)
+
+
+class ASTinterpreter:
+
+    def __init__(self, AST):
+        self.tab = 0
+        self.AST = AST
+
+    def printAST(self, branch):
+        stk = self.printStack(branch)
+        for leaf in stk:
+            if leaf == branch:
+                self.doPrint(leaf)
             else:
-                printAST(node)
+                self.printAST(leaf)
 
-def printOdr(node):
-    stk = list()
-    if node.type == "root":
-        for child in node:
-            stk.append(child)
-        return stk
-        
-    if node.type == "op":
-        if node.name == "set":
-            stk.append(node[0])
-            stk.append(node)
-            stk.append(node[1])
-            stk.append(nodeAST("synt", "\n"))
+    def printStack(self, branch):
+        stk = list()
+        if branch.type == "root":
+            for node in branch:
+                stk.append(node)
             return stk
 
-        if node.name == "in":
-            stk.append(node[0])
-            stk.append(node)
-            stk.append(node[1])
+        if branch.type == "op":
+            if branch.name == "set":
+                stk.append(branch[0])
+                stk.append(branch)
+                stk.append(branch[1])
+                stk.append(AST("synt", "\n"))
+                return stk
+
+            if branch.name in ["in", "is"]:
+                stk.append(branch[0])
+                stk.append(branch)
+                stk.append(branch[1])
+                return stk
+
+        if branch.type == "ctrl":
+            if branch.name in ["for","if"]:
+                stk.append(branch)
+                stk.append(branch[0])
+                stk.append(AST("synt", ": \n"))
+                stk.append(branch[1])
+                return stk
+
+        if branch.type == "func":
+            stk.append(branch)
+            stk.append(AST("synt", "("))
+            for child in branch:
+                stk.append(child)
+                stk.append(AST("synt", ","))
+            stk.pop()
+            stk.append(AST("synt", ")"))
             return stk
-        
-    if node.type == "ctrl":
-        if node.name == "for":
-            stk.append(node)
-            stk.append(node[0])
-            stk.append(nodeAST("synt", ": \n"))
-            stk.append(node[1])
-            return stk   
-            
-    if node.type == "func":
-        stk.append(node)
-        stk.append(nodeAST("synt", "("))
-        for child in node:
-            stk.append(child)
-            stk.append(nodeAST("synt", ","))
-        stk.pop()
-        stk.append(nodeAST("synt", ")"))
-        return stk  
-    
-    if node.type == "block":
-        stk.append(nodeAST("tab", "lvp"))
-        for child in node:
-            stk.append(nodeAST("tab", "do"))
-            stk.append(child)
-            stk.append(nodeAST("synt", "\n"))
-        stk.append(nodeAST("tab", "lvm"))    
+
+        if branch.type == "block":
+            stk.append(AST("tab", "lvp"))
+            for child in branch:
+                stk.append(AST("tab", "do"))
+                stk.append(child)
+                stk.append(AST("synt", "\n"))
+            stk.append(AST("tab", "lvm"))
+            return stk
+
+        stk.append(branch)
         return stk
-    
-    stk.append(node)
-    return stk
-    
-tab = 0
-def doPrint(node):
-    global tab
-    if node.type == "block":
-        return
-    if node.type == "tab":
-        if node.name == "lvp":
-            tab += 1
-        if node.name == "lvm":
-            tab -= 1
-        if node.name == "do":
-            for i in range(tab):
-                print("    ", end = "")
-        return
-        
-    if node.type == "synt":
-        print(getPrint(node), end = "")
-        return
-    print(getPrint(node), end = " ")
 
-def getPrint(node):
-    if node.type == "op":
-        if node.name == "set":
-            return "="
-        if node.name == "in":
-            return "in"
-    if node.type == "var":
-        return node.name
-    if node.type == "basic":
-        return node.name
-    if node.type == "ctrl":
-        if node.name == "for":
-            return node.name
-    if node.type == "func":
-        return node.name
-    if node.type == "synt":
-        return node.name
+    def doPrint(self, branch):
+        self.tab
+        if branch.type == "block":
+            return
+        if branch.type == "tab":
+            if branch.name == "lvp":
+                self.tab += 1
+            if branch.name == "lvm":
+                self.tab -= 1
+            if branch.name == "do":
+                for i in range(self.tab):
+                    print("    ", end="")
+            return
 
-    
-    
-root = nodeAST("root")
+        if branch.type == "synt":
+            print(self.getPrint(branch), end="")
+            return
+        print(self.getPrint(branch), end=" ")
+
+    def getPrint(self, branch):
+        if branch.type == "op":
+            if branch.name == "set":
+                return "="
+            if branch.name == "is":
+                return "=="
+            if branch.name == "in":
+                return "in"
+        if branch.type == "var":
+            return branch.name
+        if branch.type == "basic":
+            return branch.name
+        if branch.type == "ctrl":
+            if branch.name == "for":
+                return branch.name
+            if branch.name == "if":
+                return branch.name
+        if branch.type == "func":
+            return branch.name
+        if branch.type == "synt":
+            return branch.name
+
+
+root = AST("root")
 
 root.addChild("op", "set")
-root[0].addChild("var","i")
-root[0].addChild("basic","0")
+root[0].addChild("var", "i")
+root[0].addChild("basic", "0")
 root.addChild("ctrl", "for")
 root[1].addChild("op", "in")
-root[1][0].addChild("var","i")
+root[1][0].addChild("var", "i")
 root[1][0].addChild("func", "range")
 root[1][0][1].addChild("basic", "7")
 root[1].addChild("block")
 root[1][1].addChild("func", "print")
-root[1][1][0].addChild("var","i")
+root[1][1][0].addChild("var", "i")
 root[1][1].addChild("func", "print")
-root[1][1][1].addChild("var","i")
+root[1][1][1].addChild("var", "i")
+root.addChild("ctrl", "if")
+root[2].addChild("op", "is")
+root[2][0].addChild("var", "i")
+root[2][0].addChild("basic", "7")
+root[2].addChild("block")
+root[2][1].addChild("func", "print")
+root[2][1][0].addChild("var", "i")
 
 root.printChildren()
 print("##########")
-printAST(root)
-    
+
+itrp = ASTinterpreter(root)
+
+itrp.printAST(root)
